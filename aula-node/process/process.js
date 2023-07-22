0f380f1cf290bb47e596816cbd28adbfd11dd60e
JovemDevv <YOUR_EMAIL>
2023-07-22 13:01:30 -0300

    atualizações

---
+++ aula-node/process/process.js
@@ -0,0 +1,6 @@
+//lista de argumentos
+
+console.log('seu nome é ', process.argv[2] + '' + process.argv[3]);
+
+
+// console.log(process.argv)
---
+++ aula-node/require/aula1.js
@@ -0,0 +1,11 @@
+/* const path = require('path')
+
+console.log(path.basename("/Users/starter/docs.js"))
+
+// usa a função require para usar o modulo node
+*/
+
+const myModule = require('./require')
+
+console.log(myModule)
+// exportando de outro arquivo com EXPORTS
